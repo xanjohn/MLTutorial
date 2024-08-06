dfc8aba3cd6922e487364c1dfd0b8e782786d0b0
xanjohn <mrrizkyn123@gmail.com>
2024-08-06 21:06:41 +0700

    First Commit

---
+++ Gradient/gradient.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def gradient_descent(x,y):
+    m_curr = b_curr = 0
+    iteration = 1000
+    
+x = np.array([1,2,3,4,5])
+y = np.array([5,7,9,11,13])
+
+
+gradient_descent(x,y)
